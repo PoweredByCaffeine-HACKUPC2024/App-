7213d4efd4aadf05f6fe875a20de256bd94ad573
PauBatet <paubatet@hotmail.com>
2024-05-04 21:00:07 +0200

    Debug banner off

--- powered_by_caffein/lib/main.dart
+++ powered_by_caffein/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tiled Menu',
       theme: ThemeData(
         primarySwatch: Colors.blue,

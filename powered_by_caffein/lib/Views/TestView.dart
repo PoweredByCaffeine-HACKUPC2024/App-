325fdc2bb3d6139a0277aceb2d0ab0782f97c579
PauBatet <paubatet@hotmail.com>
2024-05-04 19:15:49 +0200

    Push tontito

--- powered_by_caffein/lib/Views/TestView.dart
+++ powered_by_caffein/lib/Views/TestView.dart
@@ -3,7 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HelpScreen extends StatefulWidget {
-  const HelpScreen({Key? key}) : super(key: key);
+  final int id;
+  const HelpScreen({super.key, required this.id});
 
   @override
   State<HelpScreen> createState() => _HelpScreenState();
@@ -24,14 +25,12 @@ class _HelpScreenState extends State<HelpScreen> {
         onPageFinished: (String url) {},
         onWebResourceError: (WebResourceError error) {},
         onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            return NavigationDecision.prevent;
-          }
           return NavigationDecision.navigate;
         },
       ),
     )
-    ..loadRequest(Uri.parse('https://flutter.dev'));
+    ..loadHtmlString(
+        '<iframe src="https://ignasidejose.grafana.net/d-solo/bdkpa3eaqkj5sb/batet-dashboard?orgId=1&from=1714814145595&to=1714835745595&panelId=1" width="450" height="200" frameborder="0"></iframe>');
 
   @override
   Widget build(BuildContext context) {
